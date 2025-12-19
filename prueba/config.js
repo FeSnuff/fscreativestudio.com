24c7883d1f6409a4f6258e48da9ab2adaba2e210
FeSnuff <46802768+FeSnuff@users.noreply.github.com>
2025-12-19 13:16:25 -0500

    Add files via upload

---
+++ prueba/config.js
@@ -0,0 +1,6 @@
+// ===== CONFIGURACIÓN GLOBAL =====
+const CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycbzSH1WBgNCmE1SfV3D0GS8_cIQirxMUsT4fIrf8Z7qMknPOVPKBLQZlNps3ttIftg4oQQ/exec'  // ← CAMBIA ESTO
+};
+
+console.log('⚙️ Config cargado');
